5e61915ae7b2d50b91adcee665d8bfaa376d1fc4
mehdigoom <medhi.gay@sfr.fr>
2019-10-22 11:40:56 +0200

    debut interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import imprime from './imprimer.png'
+import param from './parametre.png'
+import routeur from './routeur.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+      <header >
+      
+       <div className="App-header">
+        <img src={imprime} className="App-logo" alt="logo" />
+        <p className="App-txt">Imprimer</p>
+        <img src={routeur} className="App-logo" alt="logo" />
+        <img src={param} className="App-logo" alt="logo" />
+        </div>
+        
       </header>
+     
     </div>
   );
 }
